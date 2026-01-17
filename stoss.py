8f6d946fdaf6558cad396243c661b40490030375
drac28 <daswieselvomjura@gmail.com>
2026-01-17 21:42:08 +0100

    GUI

--- stoss.py
+++ stoss.py
@@ -1,8 +1,42 @@
 import matplotlib.pyplot as plt
+import PySimpleGUI as sg
 
-xnums = [5,6,7]
-ynums = [2,2,2]
+# GUI 1
+layout = [
+    [sg.Text("Anzahl Graphen:"), sg.Spin(list(range(5)))],
+    [sg.Submit(), sg.Cancel()]
+]
+window = sg.Window("Stoßtheorie - Simulation", layout)
+event, values = window.read()
+window.close()
 
+graphnum = values[0]
+
+# GUI 2
+layout_sub = []
+for i in range(graphnum):
+    frame = [
+        [sg.Spin(list(range(50)), key="-"+str(i)+"-")]
+    ]
+    layout_sub.append(sg.Frame(str(i+1)+". Graph:", frame))
+layout = [
+    [sg.Text("Wie viele Teilchen sollen pro Graph simuliert werden?")],
+    layout_sub,
+    [sg.Submit(), sg.Cancel()],
+]
+window = sg.Window("Stoßtheorie - Simulation", layout)
+event, values = window.read()
+window.close()
+
+xnums = []
+ynums = []
+
+for i in range(graphnum):
+    xnums.append(values["-"+str(i)+"-"])
+    ynums.append(2)
+
+
+# Stoss
 def plot(xnum, ynum):
     pointsold = []
     for x in range(0, xnum):
@@ -24,7 +58,7 @@ def plot(xnum, ynum):
             if point1[0] != point2[0] and point1[1] != point2[1]:
                 x = [point1[0], point2[0]]
                 y = [point1[1], point2[1]]
-                plt.plot(x, y)
+                plt.plot(x, y, color="gray")
 
 for i in range(len(xnums)):
     print(i)
